f101d310fec716e53e2494916db9621c60f00680
devmoded <matvey170906@gmail.com>
2025-04-17 18:14:24 +0300

    Изменён скрипт изменения обоев

--- .hdotsfiles/scripts/change_walls.py
+++ .hdotsfiles/scripts/change_walls.py
@@ -2,27 +2,29 @@
 
 import os
 
-walls_path = '$HOME/.hdotsfiles/wallpapers'
+walls_path = '$HOME/.hdotsfiles/wallpapers' # Путь до каталога с обоями
 
-print(f'Обои размещены в \'{walls_path}\'')
-os.system(f'ls {walls_path}')
+print(f'Обои размещены в \'{walls_path}\'') # Вывод пути до каталога с обоями
+os.system(f'ls {walls_path}') # Вывод доступных элементов в каталоге с обоями
 
-number_wall = input()
+main_input = input() # Получение введённого текста или номера
 
-if number_wall == 'list':
-    os.system(f'cat {walls_path}/.walls_summary.txt')
-    number_wall = input()
-if number_wall == 'dark':
+def set_wall(path, wall_num):
+    os.system(f'hyprctl hyprpaper preload {walls_path}/{path}/{wall_num}.png') # Презагрузка обоев
+    os.system(f'hyprctl hyprpaper wallpaper \', {walls_path}/{path}/{wall_num}.png\'') # Установка обоев
+
+if main_input == 'list':
+    os.system(f'cat {walls_path}/.walls_summary.txt') # Вывод доступных обоев с описанием в их каталогах (требуется изменять файл)
+    main_input = input()
+if main_input == 'dark':
     os.system(f'ls {walls_path}/dark')
 
-    number_wall = input()
-    os.system(f'hyprctl hyprpaper preload {walls_path}/dark/{number_wall}.png')
-    os.system(f'hyprctl hyprpaper wallpaper \', {walls_path}/dark/{number_wall}.png\'')
-if number_wall == 'light':
+    main_input = input()
+    set_wall('dark', main_input)
+if main_input == 'light':
     os.system(f'ls {walls_path}/light')
 
-    number_wall = input()
-    os.system(f'hyprctl hyprpaper preload {walls_path}/light/{number_wall}.png')
-    os.system(f'hyprctl hyprpaper wallpaper \', {walls_path}/light/{number_wall}.png\'')
+    main_input = input()
+    set_wall('light', main_input)
 else:
     print('Error')
